0c8202a39f73c531f3d0ff2eed937c33d1e4b5a1
nanogeo <hockeyplyrgeo@yahoo.com>
2024-03-31 07:41:38 -0700

    Updated cpp-sc2 to latest version

--- thirdparty/cmake/cpp_sc2.cmake
+++ thirdparty/cmake/cpp_sc2.cmake
@@ -12,7 +12,7 @@ set(BUILD_SC2_RENDERER OFF CACHE INTERNAL "" FORCE)
 FetchContent_Declare(
     cpp_sc2
     GIT_REPOSITORY https://github.com/cpp-sc2/cpp-sc2.git
-    GIT_TAG fbc081264144c266aa73bc90e52f96b3db31819d
+    GIT_TAG 5bc693e61945f99354fda448b83694421d3e3023
 )
 FetchContent_MakeAvailable(cpp_sc2)
 
