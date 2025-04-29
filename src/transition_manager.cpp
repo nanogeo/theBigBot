4f1f6065d9d536fe642d8d7ccb19272fc8422d92
nanogeo <hockeyplyrgeo@yahoo.com>
2025-04-28 18:54:37 -0700

    Changed worker rush defense to trigger with more than 2 workers instead of 4 for testing

--- src/transition_manager.cpp
+++ src/transition_manager.cpp
@@ -87,7 +87,7 @@ bool TransitionManager::WorkerRushTransitionCondition()
 {
 	// if > 4? enemy workers are within 20? of base
 	return Utility::GetUnitsWithin(mediator->GetUnits(Unit::Alliance::Enemy, IsUnits({ PROBE, SCV, DRONE })), 
-		mediator->GetStartLocation(), 20).size() > 4;
+		mediator->GetStartLocation(), 20).size() > 2;
 	
 }
 
