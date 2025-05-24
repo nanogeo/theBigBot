99f77dc0025d3c98814d6f48e6d4409b926c6ef6
nanogeo <hockeyplyrgeo@yahoo.com>
2025-05-24 08:11:37 -0700

    Balance income when defending cannon rush

--- src/build_order_manager.cpp
+++ src/build_order_manager.cpp
@@ -1217,6 +1217,7 @@ bool BuildOrderManager::CheckProtossOpening(BuildOrderResultArgData data)
 		// cannon rush
 		mediator->SendChat("Tag:scout_cannon_rush", ChatChannel::Team);
 		mediator->CreateArmyGroup(ArmyRole::cannon_rush_defense, {}, 20, 20);
+		mediator->SetBalanceIncome(true);
 		mediator->action_manager.active_actions.push_back(new ActionData(&ActionManager::ActionCheckBaseForCannons, new ActionArgData(0)));
 		mediator->action_manager.active_actions.push_back(new ActionData(&ActionManager::ActionCheckNaturalForCannons, new ActionArgData(0)));
 		Set2GateProxyRobo();
