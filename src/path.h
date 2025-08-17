993acdd5a0158cbd623ce5c67c619f9276fbd683
nanogeo <hockeyplyrgeo@yahoo.com>
2025-08-16 19:07:16 -0700

    Removed unecessary virtual function from Path

--- src/path.h
+++ src/path.h
@@ -10,7 +10,6 @@ class Path
 {
 public:
 	virtual Point2D FindClosestPoint(Point2D) const = 0;
-	virtual int FindClosestSegmentIndex(Point2D) const = 0;
 	virtual Point2D GetPointFrom(Point2D, float, bool) const = 0;
 	virtual std::vector<Point2D> FindCircleIntersection(Point2D, float) const = 0;
 	virtual std::vector<Point2D> GetPoints() const = 0;
